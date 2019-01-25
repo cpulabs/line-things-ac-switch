a86b26d426eab29dbefe103fe165a67c2dee3461
ito.takahiro <ito.takahiro@linecorp.com>
2019-01-25 16:42:39 +0900

    update

--- liff/liff.js
+++ liff/liff.js
@@ -218,7 +218,7 @@ function liffToggleDeviceLedState(state) {
     // on: 0x01
     // off: 0x00
     window.ledCharacteristic.writeValue(
-        state ? new Uint8Array([0x01]) : new Uint8Array([0x00])
+        state ? new Uint8Array([0x00]) : new Uint8Array([0x01])
     ).catch(error => {
         uiStatusError(makeErrorMsg(error), false);
     });
